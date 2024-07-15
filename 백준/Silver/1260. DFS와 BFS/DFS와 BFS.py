968ed64091f7072fb474ca39d00defccb7ac8cee
DeepSoupp <158445678+DeepSoupp@users.noreply.github.com>
2024-07-16 08:58:10 +0900

    Update DFS와 BFS.py

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,43 +1,46 @@
 from collections import deque
-
+# DFS(깊이우선탐색)  BFS(너비우선탐색)
 N, M, V = map(int, input().split())
-graph = [[] for _ in range(N + 1)]
-visited = [False] * (N + 1)
-
+# 정점 잇기 위해서 리스트 만듬
+line = [[] for _ in range(N + 1)]
+# 방문여부 체크위해 처음에 False로 채운 배열
+visit = [False] * (N + 1)
+# 간선 정보 입력
 for _ in range(M):
     a, b = map(int, input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    line[a].append(b)
+    line[b].append(a)
 
-# 각 인접 리스트를 오름차순으로 정렬
+# 각 인접 리스트를 오름차순 정렬 
 for i in range(1, N + 1):
-    graph[i].sort()
-
+    line[i].sort()
+# 방문했으면 True
 def dfs(v):
-    visited[v] = True
+    visit[v] = True
     print(v, end=' ')
-    for i in graph[v]:
-        if not visited[i]:
+    for i in line[v]:
+        # 방문 안했으면
+        if not visit[i]:
             dfs(i)
 
 def bfs(v):
     queue = deque([v])
-    visited[v] = True
+    visit[v] = True
     
     while queue:
         v = queue.popleft()
         print(v, end=' ')
-        for i in graph[v]:
-            if not visited[i]:
+        for i in line[v]:
+            if not visit[i]:
                 queue.append(i)
-                visited[i] = True
+                visit[i] = True
 
 # DFS
 dfs(V)
 print()
 
-# BFS를 위해 visited 배열을 다시 초기화
-visited = [False] * (N + 1)
+# 다시 전부 False 방문안함으로 변경
+visit = [False] * (N + 1)
 
 # BFS
 bfs(V)
