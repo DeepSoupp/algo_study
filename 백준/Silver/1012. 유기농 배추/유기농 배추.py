e4cb8ef095da6dd644bf76ae5eaee8d0c50ba053
DeepSoupp <158445678+DeepSoupp@users.noreply.github.com>
2024-07-23 00:21:45 +0900

    [Silver II] Title: 유기농 배추, Time: 336 ms, Memory: 34060 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,44 +1,34 @@
 from collections import deque
 
-# 상하좌우 이동 방향
-search = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 
-# 테스트 케이스 수 입력
+search = [(-1,0),(0,1),(1,0),(0,-1)]
+
 T = int(input())
-
 for _ in range(T):
-    M, N, K = map(int, input().split())
-    
-    # 배추밭 초기화 (행: N, 열: M)
+    M, N, k = map(int, input().split())
+    # 배추밭을 만들어주어요
     farm = [[0] * M for _ in range(N)]
-    
-    # 배추 심기
-    for _ in range(K):
+    for __ in range(k):
+        # 배추를 심어주어요
         x, y = map(int, input().split())
         farm[y][x] = 1
-    
-    # 배추흰지렁이 수
+        
     baechooWorm = 0
     
-    # 배추밭 탐색
     for i in range(N):
         for j in range(M):
-            if farm[i][j] == 1:  # 배추가 심어진 위치 발견
-                # BFS 시작
-                q = deque([(i, j)])
-                farm[i][j] = 0  # 방문한 배추는 0으로 변경
-                
+            if farm[i][j] == 1:
+                q = deque([(i,j)])
+                farm[i][j] = 0
                 while q:
-                    x, y = q.popleft()
-                    
+                    x ,y = q.popleft()
                     for sx, sy in search:
                         nx, ny = x + sx, y + sy
                         if 0 <= nx < N and 0 <= ny < M and farm[nx][ny] == 1:
-                            farm[nx][ny] = 0  # 방문한 배추는 0으로 변경
-                            q.append((nx, ny))
-                
-                # 하나의 배추흰지렁이로 모든 인접 배추를 처리
+                            farm[nx][ny] = 0
+                            q.append((nx,ny))
+                            
                 baechooWorm += 1
-    
-    # 결과 출력
+                            
+                            
     print(baechooWorm)
