6d3cf5a04747a10f9d5c7f195455ecf48c34b8ea
DeepSoupp <158445678+DeepSoupp@users.noreply.github.com>
2024-12-11 18:18:13 +0900

    [level 1] Title: 음양 더하기, Time: 0.11 ms, Memory: 9.96 MB -BaekjoonHub

--- 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -1,2 +1,8 @@
 def solution(absolutes, signs):
-    return sum(map(lambda x: x[0] if x[1] else -x[0],zip(absolutes,signs)))
+    answer = 0
+    for abs_val, sign in zip(absolutes, signs):
+        if sign:  # True이면 양수 그대로
+            answer += abs_val
+        else:  # False이면 음수
+            answer -= abs_val
+    return answer
